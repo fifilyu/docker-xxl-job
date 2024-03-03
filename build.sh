5753e556629527e43254be96271ed65aa9191fda
FifiLyu <fifilyu@gmail.com>
2024-03-03 16:00:13 +0800

    update build.sh

--- build.sh
+++ build.sh
@@ -1,1 +1,11 @@
-docker buildx build -t fifilyu/xxl-job:latest .
+tag=$(date +'%Y%m%d-%H%M%S')
+
+docker images | grep -E "^fifilyu/xxl-job   ${tag}"
+
+if [ $? -eq 0 ]; then
+    echo "[错误] 指定的Docker镜像tag已经存在：${tag}"
+    exit 1
+fi
+
+echo "[信息] 构建Docker镜像：fifilyu/xxl-job:${tag}"
+docker buildx build -t fifilyu/xxl-job:${tag} -t fifilyu/xxl-job:latest .
